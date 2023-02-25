c0132d5b42fc2e810a3c477069bd3fc1b3b5ba8e
ber <yanqinglou@outlook.com>
2023-02-24 16:24:30 -0800

    models completed

--- models/Thought.js
+++ models/Thought.js
@@ -1,16 +1,42 @@
 const { Schema, model } = require('mongoose');
 
-const reactionSchema = new Schema({
-  
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()},
+      reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 25,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
 })
 
 // Schema for what makes up a comment
-const thoughtSchema = new Schema({
-  text: String,
-  username: String,
+const thoughtSchema = new Schema(
+  {
+  text: {type:String,required:true,},
+  minLength: 15,
+  maxLength: 500,
+  createAt:{type:Date,default:Date.now()},
+  reactions:[reactionSchema]
+},
+{
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
+}  
+);
+
+// Create a virtual property `commentCount` that gets the amount of comments per post
+thoughtSchema.virtual('thoughts').get(function () {
+  return this.thoughts.length;
 });
-
-
 
 // Initialize the Comment model
 const Thought = model('thought', thoughtSchema);
--- models/User.js
+++ models/User.js
@@ -4,10 +4,10 @@ const {isEmail} = require('validator')
 // Schema to create Post model
 const userSchema = new Schema(
   {
-    name: [{type:String, unique:true, required:true, tri:true }],
-    email: [{type:String, unique:true, required:true, validate: [ isEmail, 'invalid email' ]}],
-    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
-    friends:[{ type: Schema.Types.ObjectId, ref: 'Thought' }]
+    name: {type:String, unique:true, required:true, tri:true },
+    email: {type:String, unique:true, required:true, validate: [ isEmail, 'invalid email' ]},
+    thoughts: { type: Schema.Types.ObjectId, ref: 'Thought' },
+    friends:{ type: Schema.Types.ObjectId, ref: 'User' }
   },
   {
     toJSON: {
@@ -17,10 +17,7 @@ const userSchema = new Schema(
   }  
 );
 
-// Create a virtual property `commentCount` that gets the amount of comments per post
-// userSchema.virtual('thoughts').get(function () {
-//   return this.thoughts.length;
-// });
+
 
 // Initialize our Post model
 const User = model('user', userSchema);
