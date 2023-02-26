da32455dd7229bacb94b5a28683b95f183e2494d
ber <yanqinglou@outlook.com>
2023-02-25 22:19:46 -0800

    user controller is done

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,12 +4,16 @@ const {
   getSingleUser,
   createUser,
   updateSingleUser,
+  deleteSingleUser,
 } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser).post(updateSingleUser);
+router.route('/:userId')
+.get(getSingleUser)
+.post(updateSingleUser)
+.delete(deleteSingleUser);
 
 module.exports = router;
